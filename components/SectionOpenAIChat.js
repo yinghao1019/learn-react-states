c8ccc2bdeaf70b45035a12cc09fb04116b03d9f9
andy19910102 <andy19910102@gmail.com>
2024-06-24 09:01:13 +0800

    feat: change order of openAI chat UI

--- components/SectionOpenAIChat.js
+++ components/SectionOpenAIChat.js
@@ -34,13 +34,13 @@ export default function SectionOpenAIChat() {
             <section className="relative z-10 py-14 bg-gradient-to-t from-indigo-200 from-10% via-blue-200 via-30% to-white to-90%">
                 <div className="container mx-auto">
                     <div className="">
-                        <UserMessage message={{
-                            text: "這是一段使用者跟AI的對話範例",
-                            createdAt: 1719132962547
-                        }} />
                         <AIMessage message={{
                             text: "這是一段AI回應的對話範例",
                             createdAt: 1719149969924
+                        }} />
+                        <UserMessage message={{
+                            text: "這是一段使用者跟AI的對話範例",
+                            createdAt: 1719132962547
                         }} />
                     </div>
                 </div>
