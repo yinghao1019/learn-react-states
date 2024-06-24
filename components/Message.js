aaa2b250facc4690a6a111b1a396c14488678061
andy19910102 <andy19910102@gmail.com>
2024-06-24 08:53:23 +0800

    feat: remove moment.js from default package.json

--- components/Message.js
+++ components/Message.js
@@ -1,14 +1,13 @@
-import moment from "moment";
-
-const momentFormat = "YYYY/MM/DD HH:mm:ss";
-
 export function UserMessage({ message }) {
     return (
         <div className="flex justify-end mb-3">
             <div>
                 <div className="dialog bg-gradient-to-tr from-stone-600 to-stone-800 rounded-xl rounded-tr-none shadow-lg text-white max-w-[360px] w-fit p-3">
                     <p className="text-lg mb-3 text-white">{message.text}</p>
-                    <p className="text-sm text-right text-stone-400">{moment(message.createdAt).format(momentFormat)}</p>
+                    <p className="text-sm text-right text-stone-400">
+                        {/* TODO: 將時間替換成正確的時間 */}
+                        2024/01/01 12:00:00
+                    </p>
                 </div>
             </div>
         </div>
@@ -21,7 +20,10 @@ export function AIMessage({ message }) {
             <div>
                 <div className="dialog bg-gradient-to-tr from-indigo-500 to-indigo-600 rounded-xl rounded-tl-none shadow-lg text-white max-w-[360px] w-fit p-3">
                     <p className="text-lg mb-3 text-white">{message.text}</p>
-                    <p className="text-sm text-stone-100">{moment(message.createdAt).format(momentFormat)}</p>
+                    <p className="text-sm text-stone-100">
+                        {/* TODO: 將時間替換成正確的時間 */}
+                        2024/01/01 12:00:00
+                    </p>
                 </div>
             </div>
         </div>
