9600aec1e007189fb3af8c967e88200a93b1a7e5
andy19910102 <andy19910102@gmail.com>
2024-12-22 08:46:12 +0800

    feat: add route api

---
+++ app/api/chat-ai/route.js
@@ -0,0 +1,9 @@
+// 關於next.js架構API建置規則可參考
+// https://nextjs.org/docs/app/building-your-application/routing/route-handlers#request-body
+
+export async function POST(request) {
+    // TODO: 接收並解析前端傳來的物件
+
+    // TODO: 將後端處理完成的資料回傳給前端
+
+}
