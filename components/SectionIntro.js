ec5af46e83abeea3256ca6a01c24297da4dafa6a
Ying Hao <F108157110@nkust.edu.tw>
2024-12-29 09:22:34 +0800

    feat:section、simple counter

--- components/SectionIntro.js
+++ components/SectionIntro.js
@@ -1,8 +1,10 @@
 
 // TODO: 從react模組內引入useState Hook
 // 並且定義此元件成為前端元件(只有前端元件可以使用React Hook)
-
+"use client"
+import { useState } from "react";
 import SectionTitle from "@/components/SectionTitle";
+import taiwanCities from "@/data/taiwan-cities";
 // TODO: 引入臺灣城市清單 參考 @/data/taiwan-cities
 
 export default function SectionIntro() {
@@ -11,8 +13,12 @@ export default function SectionIntro() {
     // TODO: 將城市清單渲染成放在<select>內的<option>
     // 關於HTML select tag說明文件: https://www.w3schools.com/tags/tag_select.asp
 
-    const intro = "將我替換為自我介紹內容";
-
+    
+    const [name, setName] = useState("");
+    const [city, setCity] = useState("");
+    const [occupation, setOccupation] = useState("");
+    const [copyhint, setCopyhint] = useState("");
+    const intro = `我叫${name}，我住在${city}，我是一名${occupation}。`;
     return (
         <section id="SectionIntro" className="border-b-2 py-14">
             <div className="container mx-auto px-3">
@@ -31,16 +37,23 @@ export default function SectionIntro() {
                                 type="text"
                                 className="border-2 border-slate-400 focus:border-indigo-600 w-full rounded-md text-slate-700 focus:text-indigo-600 block p-2"
                                 placeholder="在此輸入您的姓名"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
                             />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="cityInputUI" className="text-slate-700 block mb-1">居住城市</label>
                             {/* TODO: 綁定城市下拉選單的變動事件，並在發生變動時更新城市狀態 */}
+                            
                             <select
                                 id="cityInputUI"
                                 className="border-2 border-slate-400 focus:border-indigo-600 w-full rounded-md text-slate-700 block p-2"
+                                value={city}
+                                onChange={(e) => setCity(e.target.value)}
                             >
-
+                                {taiwanCities.map((city) => (
+                                    <option key={city} value={city}>{city}</option>
+                                ))}
                             </select>
                         </div>
                         <div className="mb-3">
@@ -51,6 +64,8 @@ export default function SectionIntro() {
                                 type="text"
                                 className="border-2 border-slate-400 focus:border-indigo-600 w-full rounded-md text-slate-700 focus:text-indigo-600 block p-2"
                                 placeholder="在此輸入您的職業"
+                                value={occupation}
+                                onChange={(e) => setOccupation(e.target.value)}
                             />
                         </div>
                     </div>
@@ -62,7 +77,24 @@ export default function SectionIntro() {
                             className="border-2 border-slate-400 focus:border-indigo-600 min-h-48 w-full rounded-md text-slate-700 focus:text-indigo-600 block p-3"
                             value={intro}
                             readOnly
-                        ></textarea>
+                        >
+                        </textarea>
+
+                        <button
+                            className="mt-3 px-4 py-2 bg-indigo-600 hover:bg-indigo-700 active:bg-indigo-800 text-white rounded-md"
+                            onClick={() => {
+                                navigator.clipboard.writeText(intro).then(()=>setCopyhint('已複製到剪貼簿!')).catch(()=> setCopyhint('複製失敗!更新瀏覽器'));
+                                setTimeout(()=>{
+                                    setCopyhint("");
+                                }, 3000);
+                            }}
+                        >
+                            複製到剪貼簿
+                        </button>
+
+                        <p className="text-indigo-600 mt-3">
+                            {copyhint}
+                        </p>
                     </div>
                 </div>
             </div>
--- components/SectionSimpleCounter.js
+++ components/SectionSimpleCounter.js
@@ -1,9 +1,14 @@
+"use client" 
+// 改為client component
 import SectionTitle from "@/components/SectionTitle";
-
+import { useState } from "react";
+// 當畫面需要動態改變時，我們需要使用 React State 管理
+// 使用 useState 來建立一個 React State
 export default function SectionSimpleCounter() {
     // TODO: 將要計算的數字變成是一個 React State
-
+    const [number, setNumber] = useState(0);
     // TODO: 將接觸圖片的次數變成一個 React State
+    const [touchCounts, setTouchCounts] = useState(0);
 
     return (
         <section id="SectionSimpleCounter" className="border-b-2 py-14">
@@ -14,23 +19,24 @@ export default function SectionSimpleCounter() {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     <div>
                         <h2 className="text-lg text-slate-500 mb-5">數字n現在是
-                            <span className="text-orange-500 font-semibold">5</span>。
+                            <span className="text-orange-500 font-semibold">{number}</span>。
                         </h2>
                         <div className="flex">
                             <button
                                 className="w-20 h-20 border-2 border-slate-400 rounded-l-lg block bg-slate-100 hover:bg-slate-200 active:bg-slate-300 text-slate-500 text-2xl font-semibold"
-                            // TODO: 綁定減1按鈕的點擊事件
+                        
+                                onClick={()=>setNumber(number-1)}
                             >
                                 減1
                             </button>
 
                             <div className="min-w-20 h-20 border-2 border-x-0 border-slate-400 text-2xl font-semibold flex justify-center items-center text-slate-500">
-                                5
+                                {number}
                             </div>
 
                             <button
                                 className="w-20 h-20 border-2 border-slate-400 rounded-r-lg block bg-slate-100 hover:bg-slate-200 active:bg-slate-300 text-slate-500 text-2xl font-semibold"
-                            // TODO: 綁定加1按鈕的點擊事件
+                            onClick={() => setNumber(number + 1)}
                             >
                                 加1
                             </button>
@@ -39,12 +45,13 @@ export default function SectionSimpleCounter() {
                     <div>
                         <h2 className="text-lg text-slate-500 mb-5">
                             用一個狀態值 touchCounts 代表滑鼠游標接觸圖片的次數，現在的 touchCounts 是
-                            <span className="text-orange-500 font-semibold">0</span>。
+                            <span className="text-orange-500 font-semibold">{touchCounts}</span>。
                         </h2>
                         <img
                             className="w-2/3 shadow-2xl hover:opacity-70 ease-in duration-200"
                             src="https://picsum.photos/id/908/1200/1200"
-                            alt="範例縮圖"
+                            alt="範例縮圖" 
+                            onMouseEnter={()=>setTouchCounts(touchCounts+1)}
                         />
                     </div>
                 </div>
